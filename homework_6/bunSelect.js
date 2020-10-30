bd72aec06fcab22978a1a54ecf4fe0569346eb75
Ian Thomas <yannbthomas@lil-mama.myfiosgateway.com>
2020-10-30 17:34:27 -0400

    Add button triggers alert

---
+++ homework_6/bunSelect.js
@@ -0,0 +1,27 @@
+
+/* Running total of order price */
+var totalPrice = 0
+
+/* Storing glaze type, quantity & prize when "ADD" is selected */
+
+function addBuns() {
+    var bunQuant = document.getElementById('originalQuant').value
+    var bunPrice = (bunQuant * 109) / 100
+
+    var glazes = document.getElementsByName('glaze'); 
+      
+    for(i = 0; i < glazes.length; i++) { 
+        if(glazes[i].checked) 
+        var glazeType = glazes[i].value
+    } 
+
+    document.getElementById('price').innerHTML = totalPrice + bunPrice;
+    alert(bunQuant + ' buns, price is $' + bunPrice + '. Glaze is ' + glazeType);
+    location.replace("bunSelect.html")
+}
+
+
+
+
+
+
