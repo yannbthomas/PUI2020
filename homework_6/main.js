5148464fb7b2617e3491648b287b22336d1dd3d2
Ian Thomas <yannbthomas@lil-mama.myfiosgateway.com>
2020-11-14 16:03:10 -0500

    Alert for orders too large

--- homework_6/main.js
+++ homework_6/main.js
@@ -1,5 +1,7 @@
-/* orderSize: storing number of buns selected */
 
+/*********** orderSize ************/
+
+// Storing number of buns selected
 function oneBun() {
     var orderSize = 1
     var orderSizeString = orderSize.toString()
@@ -25,28 +27,24 @@ function dozenBuns() {
     localStorage.setItem('bunsLeft', orderSizeString)
 }
 
+/*********** selectOriginal ************/
 
-// selectOriginal: Dynamically loads the order size into selectOriginal "Buns left" area
+// Dynamically loads the order size into selectOriginal "Buns left" area
 function bunLoad() {
     var orderSize = Number(localStorage.getItem('bunsLeft'))
     console.log(orderSize)
     document.getElementById("buns").innerHTML = orderSize
 }
 
-
-/* selectOriginal: Storing glaze type, quantity & price when "ADD" is selected */
+// Storing glaze type, quantity & price when "ADD" is selected 
 function addBuns() {
     var bunQuant = document.getElementById('originalQuant').value
-    
     var totalBuns = parseFloat(document.getElementById('buns').innerText)
-    
-
     var bunPrice = (bunQuant * 109) / 100
     var totalPrice = parseFloat(document.getElementById('price').innerText)
-    
-
     var glazes = document.getElementsByName('glaze'); 
       
+    // Checks popup to assign bun glazing
     for(i = 0; i < glazes.length; i++) { 
         if(glazes[i].checked) 
         var glazeType = glazes[i].value
@@ -55,19 +53,19 @@ function addBuns() {
     var popup = document.getElementById("popup");
     popup.style.display = "none";
 
-    /* Update price/quantity totals in storage */
-    var newBunsLeft = totalBuns - bunQuant
-    var newPrice = totalPrice + bunPrice
+    /* Check that customer isn't exceeding order size */
+    if (bunQuant > totalBuns) {
+        alert("Oops! Looks like that's more buns that your current order size. Head back to the Order Size tab to add more!")
+    } else {
+        var newBunsLeft = totalBuns - bunQuant
+        var newPrice = totalPrice + bunPrice
+        document.getElementById('buns').innerHTML = totalBuns - bunQuant;
+        document.getElementById('price').innerHTML = totalPrice + bunPrice;
+    }
 
-    console.log(newBunsLeft)
-    console.log(newPrice)
-    
-
-    document.getElementById('buns').innerHTML = totalBuns - bunQuant;
-    document.getElementById('price').innerHTML = totalPrice + bunPrice;
 }
 
-/* selectOriginal: Toggle the view of the ADD popup */ 
+// Toggle the view of the ADD popup
 function toggle() {
     var popup = document.getElementById("popup");
     if (popup.style.display === "block") {
