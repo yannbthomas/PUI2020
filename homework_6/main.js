6df9ab9f11740745d254605120ea03830c9959bc
Ian Thomas <yannbthomas@Lil-Mama.local>
2020-11-14 22:19:03 -0500

    Great progress, bun order stored as array...not yet appearing in the order summary

--- homework_6/main.js
+++ homework_6/main.js
@@ -29,6 +29,15 @@ function dozenBuns() {
 
 /*********** selectOriginal ************/
 
+const fullOrder = []
+
+function Bun(type, glaze, num, price) {
+    this.type = type;
+    this.glaze = glaze; 
+    this.num = num; 
+    this.price = price;
+}
+
 // Dynamically loads the order size into selectOriginal "Buns left" area
 function bunLoad() {
     var orderSize = Number(localStorage.getItem('bunsLeft'))
@@ -37,32 +46,62 @@ function bunLoad() {
 }
 
 // Storing glaze type, quantity & price when "ADD" is selected 
-function addBuns() {
+function addOrigBuns() {
+    
+    // Number of original buns, selected in the popout's number input
     var bunQuant = document.getElementById('originalQuant').value
+    // Size of order, based on selection made in bunSize
     var totalBuns = parseFloat(document.getElementById('buns').innerText)
     var bunPrice = (bunQuant * 109) / 100
+    // Collects $0.00 total price from selectOriginal
     var totalPrice = parseFloat(document.getElementById('price').innerText)
     var glazes = document.getElementsByName('glaze'); 
       
     // Checks popup to assign bun glazing
     for(i = 0; i < glazes.length; i++) { 
         if(glazes[i].checked) 
-        var glazeType = glazes[i].value
+        var glazeType = glazes[i].value;
     } 
 
+    // Check that customer isn't exceeding order size
+    if (bunQuant > totalBuns) {
+        alert("Oops! Looks like that's more buns that your current order size. Head back to the Order Size tab to add more!")
+    
+    } else { /* Core functionality */
+
+        // Remaining buns
+        var newBunsLeft = totalBuns - bunQuant;
+
+        // Add price of new original buns to the price in the order summary
+        var newPrice = totalPrice + bunPrice;
+
+        // Write in new values into selectOriginal
+        document.getElementById('buns').innerHTML = totalBuns - bunQuant;
+        document.getElementById('price').innerHTML = totalPrice + bunPrice;
+
+        // Add new bun to fulllOrder array, and store
+        var newBun = new Bun("Original", glazeType, bunQuant, bunPrice);
+        fullOrder.push(newBun);
+        localStorage.setItem("fullOrder", JSON.stringify(fullOrder));
+
+        togAndClear();
+
+    }
+}
+
+// Clear and toggle popup
+function togAndClear() {
+    var glazes = document.getElementsByName('glaze'); 
+    // Toggles popup display after clicking
     var popup = document.getElementById("popup");
     popup.style.display = "none";
 
-    /* Check that customer isn't exceeding order size */
-    if (bunQuant > totalBuns) {
-        alert("Oops! Looks like that's more buns that your current order size. Head back to the Order Size tab to add more!")
-    } else {
-        var newBunsLeft = totalBuns - bunQuant
-        var newPrice = totalPrice + bunPrice
-        document.getElementById('buns').innerHTML = totalBuns - bunQuant;
-        document.getElementById('price').innerHTML = totalPrice + bunPrice;
-    }
+    // Clear radion buttons
+    for(i=0; i < glazes.length; i++)
+        glazes[i].checked = false;
 
+    // Clear number input
+    document.getElementById('originalQuant').value = '';
 }
 
 // Toggle the view of the ADD popup
@@ -74,3 +113,16 @@ function toggle() {
       popup.style.display = "block";
     }
 }
+
+/*********** cart ************/
+
+function test() {
+    var order = JSON.parse(localStorage.getItem("fullOrder"))
+    console.log(order);
+}
+
+
+
+
+
+
